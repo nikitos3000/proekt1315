43075895d7495163f0e4d99a15729baa6ea53a70
nikita <trinevnikita@gmail.com>
2023-06-09 13:05:33 +0300

    NENAVIZY ADAPTIVNOST

--- site/map/map.js
+++ site/map/map.js
@@ -2,9 +2,15 @@ let map;
 
 DG.then(function () {
     map = DG.map('map', {
-        center: [54.98, 82.89],
-        zoom: 13
+        center: [43.585472, 39.723098],
+        zoom: 15
     });
 
-    DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([43.606076, 39.724020]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([43.622623, 39.725447]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([43.637023, 39.747160]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([43.556885, 39.773455]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([43.612499, 39.707808]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([43.582848, 39.775638]).addTo(map).bindPopup('Вы кликнули по мне!');
+   
 });

8476e730d5fd9101c516bcd2342b7a54d85a73d9
nikita <trinevnikita@gmail.com>
2023-06-16 18:08:09 +0300

    новые странички

--- site/map/map.js
+++ site/map/map.js
@@ -6,7 +6,7 @@ DG.then(function () {
         zoom: 13
     });
 
-    DG.marker([43.606076, 39.724020]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([43.606076, 39.724020]).addTo(map).bindPopup('<img src="/img2/log2.png">');
     DG.marker([43.622623, 39.725447]).addTo(map).bindPopup('Вы кликнули по мне!');
     DG.marker([43.637023, 39.747160]).addTo(map).bindPopup('Вы кликнули по мне!');
     DG.marker([43.556885, 39.773455]).addTo(map).bindPopup('Вы кликнули по мне!');

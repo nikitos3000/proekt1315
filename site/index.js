ace9c3fa877644e9459245998b755c24c46561cc
Nikolai Morachevskiy <mor97nik@mail.ru>
2023-06-28 23:04:15 +0300

    add "up" button

--- site/index.js
+++ site/index.js
@@ -128,4 +128,24 @@ setTimeout(openDialog, 20000);
 
 });
 
+const goTopBtn = document.querySelector(".go-top");
 
+window.addEventListener("scroll", trackScroll);
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+  const scrolled = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+  if (scrolled > coords) {
+    goTopBtn.classList.add("go-top--show");
+  } else {
+    goTopBtn.classList.remove("go-top--show");
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -75);
+    setTimeout(goTop, 0);
+  }
+}

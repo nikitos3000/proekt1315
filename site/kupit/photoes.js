4bb6173ec195d88984109bc5cba92f09a66cbf6d
Nikolai <mor97nik@mail.ru>
2023-06-30 21:53:27 +0300

    improve commit

--- site/kupit/photoes.js
+++ site/kupit/photoes.js
@@ -22,25 +22,3 @@ const changePhotosRight = (button) => {
 
 changePhotosRight();
 changePhotosLeft();
-
-const goTopBtn = document.querySelector(".go-top");
-
-window.addEventListener("scroll", trackScroll);
-goTopBtn.addEventListener("click", goTop);
-
-function trackScroll() {
-  const scrolled = window.pageYOffset;
-  const coords = document.documentElement.clientHeight;
-  if (scrolled > coords) {
-    goTopBtn.classList.add("go-top--show");
-  } else {
-    goTopBtn.classList.remove("go-top--show");
-  }
-}
-
-function goTop() {
-  if (window.pageYOffset > 0) {
-    window.scrollBy(0, -75);
-    setTimeout(goTop, 0);
-  }
-}
